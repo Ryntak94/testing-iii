b2a95a918b692020b82becd671467a50a6ba570b
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-02-05 21:51:06 -0500

    Initial setup of project and write first test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from '../controls/Controls.js';
+import Display from '../display/Display.js';
+
+test('Display and Controls render properly',    ()  =>  {
+    render(<Display />)
+    render(<Controls />)
+})
