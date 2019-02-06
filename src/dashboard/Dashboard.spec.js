bf35d814d17c89ea4ca9d6aa5c454d6775984c27
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-02-06 18:44:16 -0500

    Tests to make sure the close/open gate button functions properly when clicked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render, cleanup, fireEvent, getByTestId } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import Controls from '../controls/Controls.js';
 import Display from '../display/Display.js';
+import Dashboard from './Dashboard'
 
-test('Display and Controls render properly',    ()  =>  {
-    render(<Display />)
-    render(<Controls />)
+afterEach(cleanup);
+
+describe('Test to make sure Display and Controls click events work properly',   ()  =>  {
+    test('Display and Controls render properly',    ()  =>  {
+        render(<Display />)
+        render(<Controls />)
+    })
+    test('Test to make sure close/open button changes when clicked',    ()  =>  {
+        const controls = render(<Dashboard />)
+        const closeButton = controls.getByTestId(/close/i);
+        fireEvent.click(closeButton);
+        expect(closeButton).toHaveTextContent(/open gate/i);
+    })
+
+    test('Test to make sure Display shows unlocked and close when close gate button is clicked', ()  =>  {
+        const display = render(<Display />)
+        const controls = render(<Dashboard />)
+        const unlocked = display.getByText(/unlocked/i);
+        const closeButton = controls.getByTestId(/close/i);
+        fireEvent.click(closeButton);
+        const closed = display.getByText(/closed/i)
+        expect(closeButton).toHaveTextContent(/open gate/i)
+        expect(closed).toHaveTextContent(/closed/i);
+        expect(unlocked).toHaveTextContent(/unlocked/i)
+    })
+
+    test('Test to make sure displays shows unlocked and open when close gate button is clicked twice',  ()  =>  {
+        const display = render(<Display />)
+        const controls = render(<Dashboard />)
+        const unlocked = display.getByText(/unlocked/i);
+        const closeButton = controls.getByTestId(/close/i);
+        fireEvent.click(closeButton);
+        fireEvent.click(closeButton);
+        const open = display.getByText(/open/i)
+        expect(closeButton).toHaveTextContent(/close gate/i)
+        expect(open).toHaveTextContent(/open/i);
+        expect(unlocked).toHaveTextContent(/unlocked/i)
+    })
 })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+import Controls from '../controls/Controls';
+import Dashboard from '../dashboard/Dashboard';
+
+describe('Test to make sure Display functions properly',    ()  =>  {
+    const display = render(<Display />)
+    const controls = render(<Controls />)
+    const dashboard = render(<Dashboard />)
+    render(<Dashboard />);
+    test('Display renders properly',    ()  =>  {
+        render(<Display />)
+    })
+
+    test('Test to make sure Display defaults to unlocked and open', ()  =>  {
+        const unlocked = display.getByText(/unlocked/i);
+        const open = display.getByText(/open/i);
+        expect(open).toHaveTextContent(/open/i);
+        expect(unlocked).toHaveTextContent(/unlocked/i)
+    })
+})
