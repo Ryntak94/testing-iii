8746f17a9cec129ead300a3a297ffc5e827af141
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-02-06 20:50:28 -0500

    Make sure the displays are red when closed/locked

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom';
+import 'jest-dom/extend-expect';
+import Display from '../display/Display';
+import Controls from './Controls';
+import Dashboard from '../dashboard/Dashboard';
+
+describe('Test to make sure Display functions properly',    ()  =>  {
+    const display = render(<Display />)
+    const controls = render(<Controls />)
+    const dashboard = render(<Dashboard />)
+    render(<Dashboard />);
+    test('Controls renders properly',    ()  =>  {
+        render(<Controls />)
+    })
+
+    test('Test to make sure Controls buttons default to lock gate and close gate', ()  =>  {
+        const lockButton = display.getByText(/lock gate/i);
+        const closeButton = display.getByText(/close gate/i);
+        expect(lockButton).toHaveTextContent(/lock gate/i);
+        expect(closeButton).toHaveTextContent(/close gate/i)
+    })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -31,7 +31,7 @@ describe('Test to make sure Display and Controls click events work properly',   
         expect(unlocked).toHaveTextContent(/unlocked/i)
     })
 
-    test('Test to make sure displays shows unlocked and open when close gate button is clicked twice',  ()  =>  {
+    test('Test to make sure Display shows unlocked and open when close gate button is clicked twice',  ()  =>  {
         const display = render(<Display />)
         const controls = render(<Dashboard />)
         const unlocked = display.getByText(/unlocked/i);
@@ -42,5 +42,46 @@ describe('Test to make sure Display and Controls click events work properly',   
         expect(closeButton).toHaveTextContent(/close gate/i)
         expect(open).toHaveTextContent(/open/i);
         expect(unlocked).toHaveTextContent(/unlocked/i)
+    })
+
+    test('Test to make sure Display shows locked and closed when close gate and lock gate buttons are clicked', ()  =>  {
+        const display = render(<Display />);
+        const controls = render(<Dashboard />)
+        const closeButton = controls.getByText(/close gate/i);
+        const lockButton = controls.getByText(/lock gate/i);
+        fireEvent.click(closeButton);
+        fireEvent.click(lockButton);
+        const closed = display.getByText(/closed/i);
+        const locked = display.getByText(/locked/i);
+        expect(closeButton).toHaveTextContent(/open gate/i);
+        expect(lockButton).toHaveTextContent(/unlock gate/i);
+        expect(closed).toHaveTextContent(/closed/i);
+        expect(locked).toHaveTextContent(/locked/)
+    })
+
+    test('Test to make sure displays are green when unlocked and open', ()  =>  {
+        const display = render(<Display />);
+        const controls = render(<Dashboard />)
+        const open = display.getByText(/open/i)
+        const unlocked = display.getByText(/unlocked/i)
+        expect(unlocked).toHaveClass('green-led');
+        expect(open).toHaveClass('green-led');
+    })
+    test('Test to make sure displays are red when locked and closed', ()  =>  {
+        const display = render(<Display closed={true} locked={true}/>);
+        const controls = render(<Dashboard />)
+        render(<Controls />)
+        // const closeButton = controls.getByText(/close gate/i);
+        // const lockButton = controls.getByText(/lock gate/i);
+        // fireEvent.click(closeButton);
+        // fireEvent.click(lockButton);
+        const closed = display.getByText(/closed/i)
+        const locked = display.getByText(/locked/i)
+        // expect(closeButton).toHaveTextContent(/open gate/i);
+        // expect(lockButton).toHaveTextContent(/unlock gate/i);
+        expect(closed).toHaveTextContent(/closed/i)
+        expect(locked).toHaveTextContent(/locked/i)
+        expect(locked).toHaveClass('red-led');
+        expect(closed).toHaveClass('red-led');
     })
 })
